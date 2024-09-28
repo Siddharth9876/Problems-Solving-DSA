94b9c564d34c236b9fb255b7176b55e5267c1d5f
Siddharth486 <siddharthsaniyan123@gmail.com>
2024-09-29 01:07:33 +0530

    checking git working or not

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Problems-Solving-DSA
+# 1. Modular 
+# 2. DRY  (Don't repeat yourself)
+# 3. Single Responsibility principle  (like reusable components logic ko abstract karke alag function mai daalna)
+# 4. Reusability 
---
+++ index.js
@@ -0,0 +1,31 @@
+/**Q1. find the @minimum and @maximumelements in the array.**/
+
+// sabse pehle kya input lega kya output dega
+//1.  input kya lenge
+//2. output kya hoga
+
+/** 
+ * my @firstApproach absolutely @WRONG because confused about the use of @Set keyword
+ *  what does Set do?? 
+ * @param {*} arr 
+ * @returns  @array_with_unique_elements
+ */
+// poocha kya tha aur answer kya diya 
+
+
+
+/**
+ * 
+ * @param {*} arr 
+ * @returns 
+ */
+
+const minMax = (arr)=>{
+  let newArr = [...arr];
+  return newArr.sort();
+}
+
+// let arr = [3, 2, 1, 56, 10000, 167];
+
+// console.log(minMax(arr));
+// console.log(arr);
